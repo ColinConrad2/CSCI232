fd31bb208052f3c9e5156765e3640a1788b43bef
ColinConrad2 <143564535+ColinConrad2@users.noreply.github.com>
2024-03-07 13:57:18 -0700

    Update Autocomplete.java

--- CSCI232/Autocomplete.java
+++ CSCI232/Autocomplete.java
@@ -1,22 +1,46 @@
 import java.util.Arrays;
 
 public class Autocomplete {
-
-    // Initializes the data structure from the given array of terms.
+    private final Term[] terms;
     public Autocomplete(Term[] terms) {
-
+        //
+        this.terms = terms;
+        Arrays.sort(this.terms);
     }
 
     // Returns all terms that start with the given prefix, in descending order of weight.
     public Term[] allMatches(String prefix) {
-        return null; //FIXME
+        Term prefixKey = new Term(prefix, 0);// worked in mun matches can use it here
+        int firstIndexA = BinarySearchDeluxe.firstIndexOf(terms, prefixKey, Term.byPrefixOrder(prefix.length()));
+        if (firstIndexA == -1) { // Have been told I should avoid naming things same even in other methods so firstIndexA
+            return new Term[0];
+        }
+        int lastIndexA = BinarySearchDeluxe.lastIndexOf(terms,prefixKey, Term.byPrefixOrder(prefix.length()));
+        int numberOfMatches = lastIndexA -firstIndexA +1;
+        Term[] matches = Arrays.copyOfRange(terms, firstIndexA, firstIndexA +numberOfMatches);//Actual matches.
+        Arrays.sort(matches, Term.byReverseWeightOrder()); // Sort the matching terms by weight.
+
+        return matches;
     }
 
     // Returns the number of terms that start with the given prefix.
     public int numberOfMatches(String prefix) {
-        return -999; //FIXME
+        Term thePrefix = new Term(prefix, 0); // this will be my key for the Binary search
+
+        // method to find the index of the first occurence of prefixTerm in the sorted array of terms.
+        int firstIndex = BinarySearchDeluxe.firstIndexOf(terms, thePrefix, Term.byPrefixOrder(prefix.length()));
+
+        //Same but for last occurrence. Really glad there was instructions for how to call BinarySearch correct.
+        int lastIndex = BinarySearchDeluxe.lastIndexOf(terms, thePrefix, Term.byPrefixOrder(prefix.length()));
+        // -1 being returned means prefix matches nothing.
+        if (firstIndex == -1 || lastIndex == -1) {
+            return 0; // No matches found
+        }
+        //Calculate the number of matches by subtracting the index of the first occurrence from the index of the last occurrence and then adding 1.
+        return lastIndex - firstIndex + 1;
+        //Fixed?
     }
-    
+
 
     // A sample client for unit testing
     public static void main(String[] args) {
