ed0b5b8c963b6d63ecd337876cbeafdd4a1b17f0
ColinConrad2 <143564535+ColinConrad2@users.noreply.github.com>
2024-03-07 12:23:13 -0700

    BinarySearchDeluxe.java

--- CSCI232/BinarySearchDeluxe.java
+++ CSCI232/BinarySearchDeluxe.java
@@ -3,28 +3,54 @@ import java.util.Comparator;
 
 public class BinarySearchDeluxe {
 
-    
+
     // Returns the index of the first key in a[] that equals the search key, or -1 if no such key.
-    // Iterate through array comparing indecies to key until key is matched literally lets just use compare method.
     public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
-        for (int i = 0; i <a.length; i++){
-            if (comparator.compare(a[i], key)  == 0){
-                return i; //Return first occurrence of the key.
+        int low = 0;
+        int high = a.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int compr = comparator.compare(a[mid], key); // This will reduce the writing in my if statements
+            if (compr < 0) {
+                low = mid + 1;
+            }
+            else if (compr > 0) {
+                high = mid - 1;
+            }
+            else {
+                if ( mid == 0  || comparator.compare(a[mid - 1], key)!= 0){
+                    return mid; // first occurrence
+                }
+                else {
+                    high = mid -1; //Contiue search on other side
+                }
             }
         }
-        return -1; // if we get to this point then no a[i] matches key so -1
-           
+        return -1;
     }
+
+
 
     
     // Returns the index of the last key in a[] that equals the search key, or -1 if no such key.
     // So exactly the same as the last but starting from the end of our data.
     public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
-        for (int j = a.length -1; j >= 0; j-- ){
-            if (comparator.compare(a[j], key) == 0){
-                return j;
+        int low = 0;
+        int high = a.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int cmp = comparator.compare(a[mid], key);
+            if (cmp < 0) {
+                low = mid + 1;
+            } else if (cmp > 0) {
+                high = mid - 1;
+            } else {
+                if (mid == a.length - 1 || comparator.compare(a[mid + 1], key) != 0) {
+                    return mid; // Found the last occurrence
+                } else {
+                    low = mid + 1; // Continue search in the right half.
+                }
             }
-
         }
         return -1;
     }
